214713236719d296586d472e4b68120a5cd1256b
Mohanasundaram86 <105991242+Mohanasundaram86@users.noreply.github.com>
2022-08-01 18:39:38 +0530

    Update sqs_getqueueurl.js.js

--- sqs_getqueueurl.js.js
+++ sqs_getqueueurl.js.js
@@ -7,13 +7,13 @@ AWS.config.update({region: 'REGION'});
 var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
 
 var params = {
-  QueueName: 'SQS_QUEUE_NAME'
+  QueueName: 'Myqueue-proj1'
 };
 
 sqs.getQueueUrl(params, function(err, data) {
   if (err) {
     console.log("Error", err);
   } else {
-    console.log("Success", data.QueueUrl);
+    console.log("Success", data.https://sqs.us-east-1.amazonaws.com/825565961109/Myqueue-proj1);
   }
 });
